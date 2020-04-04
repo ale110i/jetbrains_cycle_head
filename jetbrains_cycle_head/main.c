f9d1b583c056ed3843f0cd8f8817e52933571bb8
Alexey Ischenko <ishhenko.as@phystech.edu>
2020-04-04 23:45:15 +0300

    Initial Commit

---
+++ jetbrains_cycle_head/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  jetbrains_cycle_head
+//
+//  Created by Alexey Ischenko on 04.04.2020.
+//  Copyright © 2020 Enthropy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
